bbe12695e6aa6a3580d7485c68c54d1ddd4b7af1
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-09-10 16:15:08 -0400

    Auto-detect github repo name when deployed.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: 'polis-param-chooser-component',
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    // This is set in the deploy.yml workflow. Otherwise, no subpath.
+    base: env.VITE_GITHUB_REPO_NAME ? `/${env.VITE_GITHUB_REPO_NAME}/` : '',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+  };
+});
