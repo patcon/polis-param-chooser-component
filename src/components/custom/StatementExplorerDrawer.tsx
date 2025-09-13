a86e5787256ab2a0b75754e7329c406dbc0e8faf
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-09-13 18:41:20 -0400

    Fixed scroll wierdness in Drawer using DrawerPortal.

--- src/components/custom/StatementExplorerDrawer.tsx
+++ src/components/custom/StatementExplorerDrawer.tsx
@@ -8,6 +8,8 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerClose,
+  DrawerPortal,
+  DrawerOverlay,
 } from "@/components/ui/drawer";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -32,40 +34,44 @@ export const StatementExplorerDrawer: React.FC<StatementExplorerDrawerProps> = (
           <MessageSquareText size={16} /> Statements
         </button>
       </DrawerTrigger>
-      <DrawerContent className="w-full max-w-full overflow-y-auto">
-        <DrawerHeader>
-          <DrawerTitle>Explore Statements</DrawerTitle>
-          <DrawerClose />
-        </DrawerHeader>
 
-        {/* Content */}
-        <div className="p-4">
-          <Tabs value={tabValue} onValueChange={setTabValue}>
-            <TabsList>
-              <TabsTrigger value="all">All</TabsTrigger>
-            </TabsList>
+      <DrawerPortal>
+        <DrawerOverlay />
+        <DrawerContent className="w-full max-w-full flex flex-col h-full">
+          <DrawerHeader>
+            <DrawerTitle>Explore Statements</DrawerTitle>
+            <DrawerClose />
+          </DrawerHeader>
 
-            <TabsContent value="all">
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>#</TableHead>
-                    <TableHead>Statement</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {statements.map((s) => (
-                    <TableRow key={s.statement_id}>
-                      <TableCell>{s.statement_id}</TableCell>
-                      <TableCell>{s.txt}</TableCell>
+          {/* scrollable content */}
+          <div className="flex-1 overflow-y-auto p-4">
+            <Tabs value={tabValue} onValueChange={setTabValue}>
+              <TabsList>
+                <TabsTrigger value="all">All</TabsTrigger>
+              </TabsList>
+
+              <TabsContent value="all">
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>#</TableHead>
+                      <TableHead>Statement</TableHead>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TabsContent>
-          </Tabs>
-        </div>
-      </DrawerContent>
+                  </TableHeader>
+                  <TableBody>
+                    {statements.map((s) => (
+                      <TableRow key={s.statement_id}>
+                        <TableCell>{s.statement_id}</TableCell>
+                        <TableCell>{s.txt}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </DrawerContent>
+      </DrawerPortal>
     </Drawer>
   );
 };
